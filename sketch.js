d802102799a98632031e34abee62152b85818b7e
Cesar1357 <85760661+Cesar1357@users.noreply.github.com>
2021-10-15 13:21:18 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 var gameState = 0;
 var lifes = 3;
 var suelo1;
-var suelo2,suelo3;
+var suelo2,suelo3,suelo4,suelo5,suelo6,suelo7,suelo8;
 var player;
 var suelo2i;
 var suelo2group;
-var enemigo;
+var enemigo,enemigo2,enemigo3;
 
 
 
@@ -22,6 +22,27 @@ function setup(){
     suelo3 = createSprite(1300,500,1000,100);
     suelo3.shapeColor = "red";
 
+    suelo4 = createSprite(3000,300,1600,100);
+    suelo4.shapeColor = "red";
+
+    suelo5 = createSprite(3000,800,1600,100);
+    suelo5.shapeColor = "red";
+
+    suelo6 = createSprite(5500,700,800,100);
+    suelo6.shapeColor = "red";
+
+    suelo7 = createSprite(6000,600,100,20);
+    suelo7.shapeColor = "red";
+    suelo7.velocityX = 10;
+
+    suelo8 = createSprite(7500,600,800,100);
+    suelo8.shapeColor = "red";
+
+
+
+
+
+
     //cosas del jugador
     player = createSprite(30,height-80,25,25);
     player.shapeColor = "black";
@@ -30,6 +51,14 @@ function setup(){
     enemigo = createSprite(1300,430,30,30);
     enemigo.shapeColor = "green"
     enemigo.velocityX = 10;
+
+    enemigo2 = createSprite(2600,215,60,80);
+    enemigo2.shapeColor = "green"
+    enemigo2.velocityX = 7;
+
+    enemigo3 = createSprite(2600,725,60,60);
+    enemigo3.shapeColor = "green"
+    enemigo3.velocityX = 20;
 
 
     suelo2group = new Group();
@@ -54,11 +83,12 @@ function draw(){
             gameState = 1;
 
         }
-
     }
     //Estado Play
 
     if(gameState === 1){
+        camera.position.x = player.x+2300;
+
         if(keyDown("a")){
             player.x = player.x - 20;
     
@@ -73,24 +103,30 @@ function draw(){
             player.velocityY = -20;
     
         }
+        player.velocityY = player.velocityY + 1;
+
         textSize(20);
         fill("black");
         text("Vidas: "+lifes,player.x+700,100);
 
-        if(player.isTouching(enemigo) || player.y > 828){
+        if(player.isTouching(enemigo) || player.isTouching(enemigo2) || player.isTouching(enemigo3) || player.y > 828){
             lifes = lifes - 1;
             player.x = 30;
             player.y = height-80;
         
         }
-
+// ayuda a comentar varias lineas /*
 
         if(lifes === 0){
             gameState = 2;
             player.visible = false;
         }
 
-        elevator();
+        elevator(500,830,100,15);
+        elevator(4100,830,100,15);
+        elevator(4600,830,100,15);
+
+
 
     }
 
@@ -99,14 +135,9 @@ function draw(){
         fill("black");
         text("Vidas: "+lifes,player.x+700,100);
         textSize(80);
-        text("Game Over",player.x+800,300);
+        text("Game Over",camera.x,300);
 
     }
-    player.collide(suelo1);
-    player.collide(suelo3);
-
-    
-
     
     if(enemigo.x > 1600){
         enemigo.velocityX = -10;
@@ -115,22 +146,55 @@ function draw(){
     if(enemigo.x < 1000){
         enemigo.velocityX = 10;
     }
+
+
+    if(enemigo2.x > 3400){
+        enemigo2.velocityX = -10;
+    }
+
+    if(enemigo2.x < 2300){
+        enemigo2.velocityX = 10;
+    }
+
+    if(enemigo3.x > 3400){
+        enemigo3.velocityX = -20;
+    }
+
+    if(enemigo3.x < 2300){
+        enemigo3.velocityX = 20;
+    }
+
+
+    if(suelo7.x > 7000){
+        suelo7.velocityX = -10;
+    }
+
+    if(suelo7.x < 6300){
+        suelo7.velocityX = 10;
+    }
     
-
-
+    
+    
+    
+    player.collide(suelo1);
+    player.collide(suelo3);
+    player.collide(suelo4);
+    player.collide(suelo5);
+    player.collide(suelo6);
+    player.collide(suelo7);
+    player.collide(suelo8);
     suelo2group.collide(player);
 
 
-    player.velocityY = player.velocityY + 1;
     drawSprites();
 
 
     //< >
 }
 
-function elevator(){
+function elevator(x,y,width,height){
     if(frameCount % 50 === 0){
-        suelo2 = createSprite(500,830,100,15);
+        suelo2 = createSprite(x,y,width,height);
         suelo2.velocityY = - 7;
         suelo2.addImage(suelo2i);
         suelo2.lifetime = 120;
